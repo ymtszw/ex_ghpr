2b0e25de7b2d0d153265444f70c8dde2d646e1be
YuMatsuzawa <celestial.rhapsody.0824@gmail.com>
2016-06-16 22:57:30 +0900

    Add deps

--- mix.exs
+++ mix.exs
@@ -6,26 +6,35 @@ defmodule ExOpenpr.Mixfile do
   def project do
     [
       app: :ex_openpr,
-       version: "0.0.1",
-       elixir: "~> 1.2",
-       build_embedded: Mix.env == :prod,
-       start_permanent: Mix.env == :prod,
-       escript: [
-         main_module: ExOpenpr.CLI,
-         name: @name,
-         path: Path.expand(Path.join(["~", ".mix", "escripts", @name])), # workaround until elixir 1.3
-       ],
-       deps: deps
+      version: "0.0.1",
+      elixir: "~> 1.2",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      escript: [
+        main_module: ExOpenpr.CLI,
+        name: @name,
+        path: Path.expand(Path.join(["~", ".mix", "escripts", @name])), # workaround until elixir 1.3
+      ],
+      deps: deps
      ]
   end
 
   def application do
-    [applications: [:logger]]
+    [
+      applications: [
+        :logger,
+        :inets,
+        :httpoison,
+      ]
+    ]
   end
 
   defp deps do
     [
-      {:croma, [github: "skirino/croma", ref: "master"]},
+      {:git_cli,   "~> 0.2"},
+      {:poison,    "~> 2.0"},
+      {:httpoison, "~> 0.8"},
+      {:croma,     "~> 0.4"},
     ]
   end
 end
