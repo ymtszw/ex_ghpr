10a8ddd82f3acdbce54ce9aec3d91c90222b6fe2
Yu Matsuzawa <celestial.rhapsody.0824@gmail.com>
2016-06-27 21:17:53 +0900

    Bump to 0.2

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule ExGHPR.Mixfile do
   def project do
     [
       app: :ex_ghpr,
-      version: "0.1.3",
+      version: "0.2.0",
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
